a52d8f335f4705ee27f2fc1f8b614787d5ff582e
daniel <mr.pokorny.d@gmail.com>
2016-10-05 13:02:22 +0200

    Pridan manual PMD

--- src/BasicAgent.java
+++ src/BasicAgent.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 /**
  * Created by daniel on 4.10.16.
  */
 public class BasicAgent {
+    public static void main(String[] args) {
+        MainWindow okno = new MainWindow();
+        okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        okno.setVisible(true);
+    }
 }
