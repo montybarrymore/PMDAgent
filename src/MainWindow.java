666a5d563dbcf07c46f7a3f72d33126a22571283
daniel <mr.pokorny.d@gmail.com>
2016-10-05 14:46:34 +0200

    Zadny vstup z klavesnice nebude! Zrusen inputText.

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -10,22 +10,14 @@ public class MainWindow extends JFrame{
         BorderLayout basePanelLayout = new BorderLayout();
         basePanel.setLayout(basePanelLayout);
 
-        JPanel leftPanel = new JPanel();
-        BorderLayout l = new BorderLayout();
-        leftPanel.setLayout(l);
-
         JTextArea outputText = new JTextArea(25,50);
         Font font = new Font("Monospaced", Font.PLAIN, 16);
         outputText.setFont(font);
         outputText.setBackground(new Color( 32,  32,  32));
         outputText.setForeground(new Color( 32, 255,  32));
-
-        JTextArea inputText = new JTextArea("inputText");
-        leftPanel.add(outputText, BorderLayout.NORTH);
-        leftPanel.add(inputText, BorderLayout.SOUTH);
-        inputText.setFont(font);
-        inputText.setBackground(new Color( 32,  32,  32));
-        inputText.setForeground(new Color( 32, 255,  32));
+        JScrollPane leftPanel = new JScrollPane(outputText);
+        leftPanel.setPreferredSize(new Dimension(500,500));
+        outputText.setLineWrap(true);
 
         JPanel rightPanel = new JPanel();
         rightPanel.setBackground(new Color(  0,   0,   0));
@@ -35,10 +27,9 @@ public class MainWindow extends JFrame{
         basePanel.add(leftPanel, BorderLayout.WEST);
         basePanel.add(rightPanel, BorderLayout.CENTER);
 
-
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Pri zmacknuti krizku zavri celou aplikaci (tj. pozabijej vsechna vlakna)
         pack();
         setVisible(true);
-
+        setResizable(false);
     }
 }
