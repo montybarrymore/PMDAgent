35347f245764b759739422ab327a5d773f41e5cb
daniel <mr.pokorny.d@gmail.com>
2017-04-18 15:23:31 +0200

    zahajen vyvoj tokenizeru

--- src/BasicAgent.java
+++ src/BasicAgent.java
@@ -1,12 +1,51 @@
-import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by daniel on 4.10.16.
  */
 public class BasicAgent {
-    public static void main(String[] args) {
-        MainWindow okno = new MainWindow();
-        okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        okno.setVisible(true);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input =br.readLine();
+        Token t = evaluate(input);
+
+        System.out.println();
+    }
+
+    public static Token evaluate(String input) {
+
+        while(input.length() > 0) {
+            String symbol = input.substring(0,1);
+
+            // oddelovac
+            if(symbol.matches("[ ,]")) {
+                input = input.substring(1);
+            } else if(symbol.matches("[0-9-]")) {
+                String substring = symbol;
+                input = input.substring(1);
+
+                boolean isNumber = true;
+                do {
+                    symbol = input.substring(0,1);
+                    if(symbol.matches("[0-9.]")) {
+                        substring = substring + symbol;
+                    } else {
+                        isNumber = false;
+                    }
+
+                    input = input.substring(1);
+
+                    if(input.length() == 0) {
+                        isNumber = false;
+                    }
+
+                } while(isNumber);
+
+                System.out.println(substring);
+            }
+        }
+        return null;
     }
 }
---
+++ src/Token.java
@@ -0,0 +1,6 @@
+/**
+ * @version 0.0.0
+ *          Created by daniel on 18.4.17.
+ */
+public class Token {
+}
