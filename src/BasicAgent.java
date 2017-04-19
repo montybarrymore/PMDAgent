b27a10a4dff86dd939668341344fe8aba06b5405
daniel <mr.pokorny.d@gmail.com>
2017-04-19 12:49:11 +0200

    pokracuje vyvoj tokenizeru

--- src/BasicAgent.java
+++ src/BasicAgent.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /**
  * Created by daniel on 4.10.16.
@@ -15,37 +16,76 @@ public class BasicAgent {
     }
 
     public static Token evaluate(String input) {
+        ArrayList<Token> infixList = new ArrayList<>();
 
-        while(input.length() > 0) {
-            String symbol = input.substring(0,1);
+        if (input.length() > 0) {
+            int index = 0;
+            do {
+                String symbol = input.substring(index, index + 1);
+                // oddelovac
+                if (symbol.matches("[ ,]")) {
+                    index++;
+                    System.out.println(symbol);
+                }
+                // NUMBER
+                else if(symbol.matches("-") && input.substring(index + 1, index + 2).matches("[0-9]") ||
+                        symbol.matches("[0-9]")) {
+                    String substring = symbol;
+                    index++;
 
-            // oddelovac
-            if(symbol.matches("[ ,]")) {
-                input = input.substring(1);
+                    boolean isNumber = true;
+                    do {
+                        if(index < input.length()) {
+                            symbol = input.substring(index, index + 1);
+                            if (symbol.matches("[0-9.]")) {
+                                substring = substring + symbol;
+                            } else {
+                                isNumber = false;
+                            }
+                            index++;
+                        } else {
+                            isNumber = false;
+                        }
+                    } while(isNumber);
+                    infixList.add(new Token(Token.Type.NUMBER, Float.parseFloat(substring)));
+
+                // ostatni
+                } else {
+                    index++;
+                }
+            } while (index < input.length()) ;
+        }
+        return null;
+    }
+}
+/*
+            // cislo
             } else if(symbol.matches("[0-9-]")) {
                 String substring = symbol;
                 input = input.substring(1);
 
                 boolean isNumber = true;
                 do {
-                    symbol = input.substring(0,1);
-                    if(symbol.matches("[0-9.]")) {
-                        substring = substring + symbol;
+                    if(input.length() > 0) {
+                        symbol = input.substring(0, 1);
+                        if (symbol.matches("[0-9.]")) {
+                            substring = substring + symbol;
+                        } else {
+                            isNumber = false;
+                        }
+                        input = input.substring(1);
                     } else {
                         isNumber = false;
                     }
-
-                    input = input.substring(1);
-
-                    if(input.length() == 0) {
-                        isNumber = false;
-                    }
-
                 } while(isNumber);
-
-                System.out.println(substring);
+                infixList.add(new Token(Token.Type.NUMBER, Float.parseFloat(substring)));
             }
         }
-        return null;
-    }
-}
+
+        for(Token t : infixList) {
+            if(t.isNumber()) {
+                System.out.println(t.getValue());
+            }
+        }
+        System.out.println(infixList);
+        */
--- src/Token.java
+++ src/Token.java
@@ -3,4 +3,27 @@
  *          Created by daniel on 18.4.17.
  */
 public class Token {
+    public enum Type {
+        NUMBER, STRING
+    }
+
+    private Type type_;
+    private Object value_;
+
+    public Token(Type type, Object value) {
+        type_ = type;
+        value_ = value;
+    }
+
+    public boolean isNumber() {
+        if(type_ == Type.NUMBER) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String getValue() {
+        return value_.toString();
+    }
 }
