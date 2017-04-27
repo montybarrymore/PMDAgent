8785baffb3d4b6f393779711651c39ec6adb593a
daniel <mr.pokorny.d@gmail.com>
2017-04-27 15:22:44 +0200

    scita float, udelat scitani tokenu.

--- src/BasicAgent.java
+++ src/BasicAgent.java
@@ -40,11 +40,12 @@ public class BasicAgent {
      * @return
      */
     public static Float evaluate(String input) {
+        Float returnValue = null;
         if(input.length() > 0) {
             int bracketCount = 0;
             int dividerPosition = -1;
             int dividerPriority = 1000;
-            Operator operator;
+            Operator operator = null;
 
             for (int i = 0; i < input.length() - 2; i++) {
                 String chr = input.substring(i, i + 1);
@@ -73,64 +74,16 @@ public class BasicAgent {
                 String right = input.substring(dividerPosition + 2);
                 switch (operator) {
                     case PLUS:
-
+                        returnValue = evaluate(left) + evaluate(right);
                 }
 
                 System.out.println();
             } else {
+                returnValue = Float.parseFloat(input);
                 System.out.println(input);
             }
         }
-/*        ArrayList<Token> infixList = new ArrayList<>();
 
-        if (input.length() > 0) {
-            int index = 0;
-            do {
-                String symbol = input.substring(index, index + 1);
-                // oddelovac
-                if (symbol.matches("[ ,]")) {
-                    index++;
-                    System.out.println(symbol);
-                }
-
-                // NUMBER
-                else if (symbol.matches("-") && input.substring(index + 1, index + 2).matches("[0-9]") ||
-                        symbol.matches("[0-9]")) {
-                    String substring = symbol;
-                    index++;
-
-                    boolean isNumber = true;
-                    do {
-                        if (index < input.length()) {
-                            symbol = input.substring(index, index + 1);
-                            if (symbol.matches("[0-9.]")) {
-                                substring = substring + symbol;
-                            } else {
-                                isNumber = false;
-                            }
-                            index++;
-                        } else {
-                            isNumber = false;
-                        }
-                    } while (isNumber);
-                    infixList.add(new Token(Token.Type.NUMBER, Float.parseFloat(substring)));
-                }
-
-                // plus
-                else if (symbol.equals("+")) {
-                    infixList.add(new Token(Token.Type.PLUS, null));
-                    index++;
-                }
-
-                // ostatni
-                else {
-                    index++;
-                }
-            } while (index < input.length());
-        }
-
-
-*/
-        return null;
+        return returnValue;
     }
 }
