1e6c19e236fd85657431029a660bbfba8cddb392
daniel <mr.pokorny.d@gmail.com>
2017-04-27 15:09:58 +0200

    evaluate zahajen rekurzi.

--- src/BasicAgent.java
+++ src/BasicAgent.java
@@ -1,22 +1,87 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 /**
  * Created by daniel on 4.10.16.
  */
 public class BasicAgent {
+    enum Operator {PLUS};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
-        Token t = evaluate(input);
+        Float t = evaluate(input);
 
         System.out.println();
     }
 
-    public static Token evaluate(String input) {
-        ArrayList<Token> infixList = new ArrayList<>();
+    /**
+     * Priority operatoru
+     * 1. výrazy v zátvorkách     ( )
+     * 2. funkcie
+     * 3. umocnenie ^
+     * 4. polarita + -
+     * 5. násobenie *
+     *    delenie /
+     * 6. sčítanie +
+     *    odčítanie -
+     * 7. relácia "je rovný" =
+     *    relácia "je menší" <
+     *    relácia "je väčší" >
+     *    relácia "je rovný alebo menší" <=
+     *    relácia "nerovná sa" <>
+     *    relácia "je rovný alebo väčší" >=
+     * 8. dvojkový komplement NOT
+     * 9. logický súčin AND
+     *    logický súčet OR
+     *
+     * @param input
+     * @return
+     */
+    public static Float evaluate(String input) {
+        if(input.length() > 0) {
+            int bracketCount = 0;
+            int dividerPosition = -1;
+            int dividerPriority = 1000;
+            Operator operator;
+
+            for (int i = 0; i < input.length() - 2; i++) {
+                String chr = input.substring(i, i + 1);
+                if(chr.equals("(")) {
+                    bracketCount++;
+                }
+                if(chr.equals(")")) {
+                    bracketCount--;
+                }
+
+                if(bracketCount == 0) {
+                    String trigram = input.substring(i, i + 3);
+                    if(trigram.equals(" + ")) {
+                        if(dividerPriority >= 6) {
+                            dividerPosition = i + 1;
+                            dividerPriority = 6;
+                            operator = Operator.PLUS;
+                        }
+                    }
+                }
+
+            }
+
+            if(dividerPriority < 1000) {
+                String left = input.substring(0, dividerPosition - 1);
+                String right = input.substring(dividerPosition + 2);
+                switch (operator) {
+                    case PLUS:
+
+                }
+
+                System.out.println();
+            } else {
+                System.out.println(input);
+            }
+        }
+/*        ArrayList<Token> infixList = new ArrayList<>();
 
         if (input.length() > 0) {
             int index = 0;
@@ -65,7 +130,7 @@ public class BasicAgent {
         }
 
 
-
+*/
         return null;
     }
 }
